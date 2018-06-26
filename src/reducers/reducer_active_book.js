1f4d1661803fad76ff16ef13e5916d71b1333217
Brendon Kuleck <brendonkuleck@gmail.com>
2018-06-26 14:35:42 -0500

    Gone ye barnicles

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,9 +1,10 @@
 // State arugment is not application state, only the state this reducer is responsible for
 
-export default function(state = null, action){
+export default class (state = null, action){
   switch(action.type){
     case 'BOOK_SELECTED':
       return action.payload;
+    }
   }
   return state;
 }
