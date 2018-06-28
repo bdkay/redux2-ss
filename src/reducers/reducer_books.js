fcb1e7b2881d7777ff1f8ad7326616ca4da4c758
Brendon Kuleck <brendonkuleck@gmail.com>
2018-06-28 10:19:49 -0500

    Reducers in place

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function(){
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Dark Tower' },
-    { title: 'IT' }
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 400 },
+    { title: 'The Dark Tower', pages: 1600 },
+    { title: 'IT Script', pages: 100 }
   ]
 }
